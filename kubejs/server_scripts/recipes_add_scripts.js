787490e3de64cde56ab7d19e14f4c6e3f8cf4ada
Lanonymous <163461193+RLLanonymous@users.noreply.github.com>
2024-09-11 20:16:44 +0200

    Update recipes_add_scripts.js

--- kubejs/server_scripts/recipes_add_scripts.js
+++ kubejs/server_scripts/recipes_add_scripts.js
@@ -9,16 +9,29 @@ ServerEvents.recipes(event => {
         'kubejs:id4here',
     ])
 })
-
+*/
+// Clock Coin Recipe
 ServerEvents.recipes(event => {
-//kubejs = mod ; idhere = nom de l'objet/item a craft.
-//Ceux du dessous sont les item nécessaire a la recipe, avec la précision de la forme nécessaire.
-    event.shaped('kubejs:idhere', [
-        'AAA',
-        'ABA',
-        'A A'
+    event.shaped('kubejs:clock_coin', [
+        'ACA',
+        'CBC',
+        'ACA'
     ],{
-        A: 'kubejs:id',
-        B: 'minecraft:id'
+        A: 'minecraft:gold_ingot',
+        B: 'kubejs:broken_clock_coin',
+        C: 'minecraft:diamond'
     })
-}) */
+})  
+// Broken Clock Coin Recipe
+ServerEvents.recipes(event => {
+    event.shaped('kubejs:broken_clock_coin', [
+        'DAD',
+        'CBC',
+        'DAD'
+    ],{
+        A: 'mekanism:alloy_atomic',
+        B: 'minecraft:echo_shard',
+        C: 'minecraft:netherite_ingot',
+        D: 'minecraft:gold_ingot'
+    })
+})  
