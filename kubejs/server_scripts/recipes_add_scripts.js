138a25a4168c8daebed575172fb70d913ac6b251
Lanonymous <163461193+RLLanonymous@users.noreply.github.com>
2024-09-14 20:11:25 +0200

    Add files via upload

--- kubejs/server_scripts/recipes_add_scripts.js
+++ kubejs/server_scripts/recipes_add_scripts.js
@@ -1,15 +1,3 @@
-/* //Recipes Add
-
-ServerEvents.recipes(event => {
-//kubejs = mod ; idhere = nom de l'objet/item a craft.
-//Ceux du dessous sont les item nécessaire a la recipe
-    event.shapeless('kubejs:idhere', [
-        'kubejs:id2here',
-        'kubejs:id3here',
-        'kubejs:id4here',
-    ])
-})
-*/
 // Clock Coin Recipe
 ServerEvents.recipes(event => {
     event.shaped('kubejs:clock_coin', [
@@ -25,25 +13,37 @@ ServerEvents.recipes(event => {
 // Broken Clock Coin Recipe
 ServerEvents.recipes(event => {
     event.shaped('kubejs:broken_clock_coin', [
-        'DAD',
-        'CBC',
-        'DAD'
-    ],{
-        A: 'mekanism:alloy_atomic',
-        B: 'minecraft:echo_shard',
-        C: 'minecraft:netherite_ingot',
-        D: 'minecraft:gold_ingot'
-    })
-})  
-// Clock Star Recipe
-ServerEvents.recpies(event => {
-    event.shaped('kubejs:clock_star', [
+        'BBB',
         'BAB',
-        'ACA',
-        'BAB'
+        'BBB'
     ],{
-        A: 'minecraft:netherite_block',
-        B: 'kubejs:clock_coin',
-        C: 'minecraft:nether_star'
+        A: 'minecraft:gold_block',
+        B: 'minecraft:gold_ingot'
     })
+}) 
+// Ultimate Clock Star Recipe
+ServerEvents.recipes(event => {
+    event.shapeless('kubejs:ultimate_clock_star', [
+        'kubejs:clock_star',
+        'kubejs:clock_star',
+        'kubejs:clock_star',
+        'kubejs:clock_star',
+        'kubejs:clock_star',
+        'kubejs:clock_star',
+        'kubejs:clock_star',
+        'kubejs:clock_star',
+        'kubejs:clock_star'
+    ])
 })
+
+// ClockSword Recipe
+ServerEvents.recipes(event => {
+    event.shaped('kubejs:clock_sword', [
+        ' A ',
+        ' B ',
+        ' B '
+    ],{
+        A: 'kubejs:ultimate_clock_star',
+        B: 'minecraft:stick'
+    })
+}) 
