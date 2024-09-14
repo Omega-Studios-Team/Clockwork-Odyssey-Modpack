d8ef7b8e8308b5776777d4790a0545d71ac78923
Lanonymous <163461193+RLLanonymous@users.noreply.github.com>
2024-09-14 20:10:57 +0200

    Add files via upload

--- kubejs/startup_scripts/item_add_scripts.js
+++ kubejs/startup_scripts/item_add_scripts.js
@@ -1,24 +1,37 @@
-// ClockCoin
+// Clock Coin
 StartupEvents.registry('item', event => {
-    event.create('clock_coin')
-    .displayName('Clock Coin')
-    item.maxStackSize = 8
-    item.rarity = "RARE"
+    event.create('clock_coin').displayName(('\u00A76' + 'Clock Coin'))
 })
 
-// BrokenClockCoin
+// Broken Clock Coin
 StartupEvents.registry('item', event => {
-    event.create('broken_clock_coin')
-    .displayName('Broken Clock Coin')
-    item.maxStackSize = 16
-    item.rarity = "RARE"
+    event.create('broken_clock_coin').displayName(('\u00A76' + 'Broken Clock Coin'))
 })
 
-// Clock Star 
+// Clock Star
 StartupEvents.registry('item', event => {
-    event.create('clock_star')
-    .displayName('Clock Star')
-    item.maxStackSize = 64
-    item.rarity = "EPIC"
+    event.create('clock_star').displayName(('\u00A7d' + 'Clock Star'))
 })
 
+// Ultimate Clock Star 
+StartupEvents.registry('item', event => {
+    event.create('ultimate_clock_star').displayName(('\u00A75' + 'Ultimate Clock Star'))
+})
+
+// Clock Tier
+ItemEvents.toolTierRegistry(event => {
+    event.add('clock', tier => {
+        tier.uses = 10000
+        tier.speed = 20.0
+        tier.attackDamageBonus = 4.0
+        tier.level = 5
+        tier.enchantmentValue = 14
+        tier.repairIngredient = 'kubejs:clock_star'
+    })
+})
+
+// ClockSword
+StartupEvents.registry('item', event => {
+    event.create('clock_sword', 'sword').tier('clock').attackDamageBaseline(120.0).displayName(('\u00A7e' + 'The Clock Sword'))
+})
+    
